dfbf4e1b5c57177d7b46cf998690c4ab02214c79
kevinlhoste <kevin.lhoste@gmail.com>
2017-05-17 11:15:54 +0800

    changed neopixel code addes oled (not tested)

--- Firmware/Unit_test/Neopixel/Neopixel.ino
+++ Firmware/Unit_test/Neopixel/Neopixel.ino
@@ -5,14 +5,14 @@
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
-#define PIN           15//gpio12
+#define PIN           14//gpio12
 //gpio15 OK
 //gpio2 OK
 //gpio14 OK
 //gpio12 BAD :-(
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS     1
+#define NUMPIXELS     16
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -42,8 +42,16 @@ void loop() {
     pixels.show(); // This sends the updated pixel color to the hardware.
 
     delay(delayval); // Delay for a period of time (in milliseconds).
-    pixels.setPixelColor(i, pixels.Color(150,150,0)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
     pixels.show(); 
+    delay(delayval);
+  }
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(150,150,150)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware. 
     delay(delayval);
   }
 }
